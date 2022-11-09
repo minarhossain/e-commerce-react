0dc0b3324e959e5422adc7ea7b541e40be8d08cb
minarhossain <codewithmnr@gmail.com>
2022-11-09 23:23:27 +0600

    cart component create and passing data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 
 import './Shop.css'
@@ -34,8 +35,7 @@ const Shop = () => {
 
             </div>
             <div className="cart-container yes2">
-                <h3>Order Summary </h3>
-                <p>Selected Items {cart.length}</p>
+                <Cart cart={cart}></Cart>
                 
             </div>
         </div>
